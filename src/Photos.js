6670d3cb40cd4c034949dca8d3dfa7c5cc0f9113
eutinawei <46619191+etwyt@users.noreply.github.com>
2023-07-31 20:09:10 -0600

    Add photos and lightbox pages

---
+++ src/Photos.js
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import Lightbox from "./components/Lightbox";
+import { forMobile } from "./constants/breakpoints";
+import cities from "./constants/cities";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  position: absolute;
+  top: 120px;
+  left: 120px;
+  width: calc(100vw - 180px);
+  height: calc(100vh - 180px);
+  overflow: scroll;
+  ${forMobile} {
+    top: 100px;
+    left: 40px;
+    width: calc(100vw - 80px);
+    height: calc(100vh - 140px);
+  }
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+const Image = styled.img`
+  width: calc(100vw / 3 - 80px);
+  ${forMobile} {
+    width: calc(100vw - 80px);
+  }
+  height: auto;
+  margin-bottom: 25px;
+  &:hover {
+    cursor: zoom-in;
+  }
+`;
+
+const Photos = () => {
+  const params = useParams();
+  let name = "";
+  let photoCount = 0;
+  for (const city of cities) {
+    if (city.key === params.key) {
+      name = city.key;
+      photoCount = city.photoCount;
+      break;
+    }
+  }
+
+  const [lightboxImage, setLightboxImage] = useState("");
+  const [width, setWidth] = useState(window.innerWidth);
+  window.addEventListener("resize", () => setWidth(window.innerWidth));
+
+  const openLightbox = (key) => {
+    setLightboxImage(key);
+  };
+
+  let imagesCol0 = [];
+  let imagesCol1 = [];
+  let imagesCol2 = [];
+  let imagesColMobile = [];
+
+  let row0Count = Math.floor(photoCount / 3);
+  let row1Count = Math.floor(photoCount / 3);
+
+  let remainer = photoCount % 3;
+  if (remainer === 1) {
+    row0Count += 1;
+  } else if (remainer === 2) {
+    row0Count += 1;
+    row1Count += 1;
+  }
+
+  for (let i = 0; i < row0Count; i++) {
+    imagesCol0.push(
+      <Image
+        key={name + "_" + i}
+        src={require("./assets/photos/" + name + "/" + i + ".jpg")}
+        onClick={() => openLightbox(name + "_" + i)}
+      />
+    );
+  }
+  for (let i = row0Count; i < row0Count + row1Count; i++) {
+    imagesCol1.push(
+      <Image
+        key={name + "_" + i}
+        src={require("./assets/photos/" + name + "/" + i + ".jpg")}
+        onClick={() => openLightbox(name + "_" + i)}
+      />
+    );
+  }
+  for (let i = row0Count + row1Count; i < photoCount; i++) {
+    imagesCol2.push(
+      <Image
+        key={name + "_" + i}
+        src={require("./assets/photos/" + name + "/" + i + ".jpg")}
+        onClick={() => openLightbox(name + "_" + i)}
+      />
+    );
+  }
+  for (let i = 0; i < photoCount; i++) {
+    imagesColMobile.push(
+      <Image
+        key={name + "_" + i}
+        src={require("./assets/photos/" + name + "/" + i + ".jpg")}
+        onClick={() => openLightbox(name + "_" + i)}
+      />
+    );
+  }
+
+  return (
+    <Wrapper>
+      {lightboxImage && (
+        <Lightbox image={lightboxImage} setLightboxImage={setLightboxImage} />
+      )}
+      {width >= 1024 ? (
+        <>
+          <Column>{imagesCol0}</Column>
+          <Column>{imagesCol1}</Column>
+          <Column>{imagesCol2}</Column>
+        </>
+      ) : (
+        <Column>{imagesColMobile}</Column>
+      )}
+    </Wrapper>
+  );
+};
+
+export default Photos;
---
+++ src/components/Lightbox.js
@@ -0,0 +1,81 @@
+import React, { useRef } from "react";
+import styled, { keyframes } from "styled-components";
+import { forDesktop, forMobile } from "../constants/breakpoints";
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+const Wrapper = styled.div`
+  z-index: 10;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Background = styled.button`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(255, 255, 255, 0.7);
+  animation: ${fadeIn} 0.5s;
+  border: 0;
+  &:hover {
+    cursor: zoom-out;
+  }
+`;
+
+const Image = styled.img`
+  z-index: 15;
+  width: auto;
+  height: auto;
+  object-fit: cover;
+  ${forDesktop} {
+    max-width: 70%;
+    max-height: 80%;
+  }
+  ${forMobile} {
+    max-width: 80%;
+    max-height: 80%;
+  }
+`;
+
+const Lightbox = ({ image, setLightboxImage }) => {
+  const exitRef = useRef();
+
+  const mouseEnter = (ref) => {
+    ref.current.style.fontWeight = "bold";
+  };
+
+  const mouseLeave = (ref) => {
+    ref.current.style.fontWeight = "normal";
+  };
+
+  return (
+    <Wrapper>
+      <Background
+        ref={exitRef}
+        onMouseEnter={() => mouseEnter(exitRef)}
+        onMouseLeave={() => mouseLeave(exitRef)}
+        onClick={() => setLightboxImage("")}
+      />
+      <Image
+        src={require(`../assets/photos/${image.split("_")[0]}/${
+          image.split("_")[1]
+        }.jpg`)}
+      />
+    </Wrapper>
+  );
+};
+
+export default Lightbox;
